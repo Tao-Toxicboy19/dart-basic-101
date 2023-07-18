91dd2eb5d0bb0afda04868a68d8bb4193f34ed16
Tao-Toxicboy19 <ppv_tao@outlook.co.th>
2023-07-18 14:16:28 +0700

    function parameters

--- main.dart
+++ main.dart
@@ -3,7 +3,8 @@ void main() {
 }
 
 void app() {
-  myFunctionV1();
+  // MyFunctionV3(523.22, 5454.111, "Hello");
+  MyfunctionV5("Hello Prawit");
 }
 
 void myFunctionV1() {
@@ -15,4 +16,21 @@ void myFunctionV1() {
 void myFunctionV2() {
   double myMoney = 999.99;
   print("I have money = ${myMoney}");
+}
+
+// parameters no return type
+
+void MyFunctionV3(double x, double y, String z) {
+  print("x + y = ${x + y}");
+  print("z = ${z}");
+
+  MyFunctionV4(x + y);
+}
+
+void MyFunctionV4(double sum) {
+  print("Som of X + Y = ${sum}");
+}
+
+void MyfunctionV5(String name) {
+  print("${name.toUpperCase()}");
 }
