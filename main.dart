10eddc10612c0f41870fc9a95792e0d0389699db
Tao-Toxicboy19 <ppv_tao@outlook.co.th>
2023-07-18 14:00:22 +0700

    functionV2

--- main.dart
+++ main.dart
@@ -3,8 +3,16 @@ void main() {
 }
 
 void app() {
-  String stdName = "thewarat";
-  print("I am ${stdName} in my function");
+  myFunctionV1();
 }
 
+void myFunctionV1() {
+  String stdName = "thewarat";
+  print("I am ${stdName} in my function");
+  myFunctionV2();
+}
 
+void myFunctionV2() {
+  double myMoney = 999.99;
+  print("I have money = ${myMoney}");
+}
