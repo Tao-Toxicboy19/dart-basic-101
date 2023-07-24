5127726dafa041271ddb4be15754a6066ab21bb3
Tao-Toxicboy19 <ppv_tao@outlook.co.th>
2023-07-25 06:51:39 +0700

    funtion

--- class-04/main.dart
+++ class-04/main.dart
@@ -3,5 +3,14 @@ void main() {
 }
 
 app() {
-  print("Hello");
+  // adder(1, 2, 2, "hello");
+  suntract(w:20,x:20,y:10);
+}
+
+adder(int a, int b, int c, [String? x = "บวก"]) {
+  print("${a + b + c} $x");
+}
+
+suntract({int w = 0, int x = 0, int y = 0}) {
+  print(w - x - y);
 }
