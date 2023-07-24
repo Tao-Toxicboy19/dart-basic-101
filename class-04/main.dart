2b0926209d2e7159e29192e6cb9e6853676daad5
Tao-Toxicboy19 <ppv_tao@outlook.co.th>
2023-07-25 06:54:39 +0700

    Required

--- class-04/main.dart
+++ class-04/main.dart
@@ -4,13 +4,13 @@ void main() {
 
 app() {
   // adder(1, 2, 2, "hello");
-  suntract(w:20,x:20,y:10);
+  suntract(w: 20, x: 20, y: 10);
 }
 
 adder(int a, int b, int c, [String? x = "บวก"]) {
   print("${a + b + c} $x");
 }
 
-suntract({int w = 0, int x = 0, int y = 0}) {
+suntract({required int w, int x = 0, int y = 0}) {
   print(w - x - y);
 }
