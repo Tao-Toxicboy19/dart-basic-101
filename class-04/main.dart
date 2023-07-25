3e651b8469eae5c95fec3c92c3cbf317fb54917d
Tao-Toxicboy19 <ppv_tao@outlook.co.th>
2023-07-25 07:53:59 +0700

    nonymous function

--- class-04/main.dart
+++ class-04/main.dart
@@ -4,7 +4,22 @@ void main() {
 
 app() {
   // adder(1, 2, 2, "hello");
-  suntract(w: 20, x: 20, y: 10);
+  // suntract(w: 20, x: 20, y: 10);
+  // showdata(name: "Prayut", studentId: "123456", age: 20, nickName: "Tao");
+  // print(check(weight: 200));
+  // (check(weight: 200)) ? print("ไอ้อ้วน") : print("เบิ่งดี");
+  // print(createList());
+  // String fullName({String? firstname, String? lastname}) {
+  //   return (firstname ?? '') + (lastname ?? '');
+  // }
+
+  // print(fullName(firstname: "Prayut", lastname: "Chanocha"));
+
+  List<int> myScore = [70, 55, 65, 80];
+  myScore.forEach((int m) {
+    // print("My Score ${m}");
+    (m >= 70) ? print("Nice one Guys ${m}") : print("Noob Guys ${m}");
+  });
 }
 
 adder(int a, int b, int c, [String? x = "บวก"]) {
@@ -13,4 +28,25 @@ adder(int a, int b, int c, [String? x = "บวก"]) {
 
 suntract({required int w, int x = 0, int y = 0}) {
   print(w - x - y);
+}
+
+showdata(
+    {String? name,
+    required String studentId,
+    int? age,
+    required String nickName}) {
+  print("StudenId : ${studentId}");
+  print("Name : ${name}");
+  print("Nickname : ${nickName}");
+  print("Age : ${age}");
+}
+
+bool check({int? weight}) {
+  bool fat = (weight! >= 75) ? true : false;
+  return fat;
+}
+
+List<int> createList() {
+  List<int> a = [1, 2, 3, 4, 5];
+  return a;
 }
