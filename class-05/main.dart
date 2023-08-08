bb7808f9b25649901d19b036ba27cb100374afb6
Tao-Toxicboy19 <ppv_tao@outlook.co.th>
2023-08-08 13:42:43 +0700

    class 1

--- class-05/main.dart
+++ class-05/main.dart
@@ -1,3 +1,23 @@
 void main() {
-  print("hello");
+  Student std_1 = Student();
+  std_1.name = ("John");
+  std_1.age = (15);
+  std_1.department = ("CAE");
+  std_1.viewStudent();
+  /////////////////////////////
+  Student std_2 = Student();
+  std_2.name = ("John Doe");
+  std_2.age = (11);
+  std_2.department = ("ML");
+  std_2.viewStudent();
+}
+
+class Student {
+  String? name;
+  int? age;
+  String? department;
+
+  viewStudent() {
+    print("Hello ${name} Age ${age} Department ? ${department}");
+  }
 }
