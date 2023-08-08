2c75625f19b9546a179b22d448dbed1514a32488
Tao-Toxicboy19 <ppv_tao@outlook.co.th>
2023-08-08 14:02:22 +0700

    Class Animal

--- class-05/main.dart
+++ class-05/main.dart
@@ -1,15 +1,34 @@
+import 'dart:math';
+
 void main() {
   Student std_1 = Student();
   std_1.name = ("John");
   std_1.age = (15);
   std_1.department = ("CAE");
-  std_1.viewStudent();
+  // std_1.viewStudent();
   /////////////////////////////
   Student std_2 = Student();
   std_2.name = ("John Doe");
   std_2.age = (11);
   std_2.department = ("ML");
-  std_2.viewStudent();
+  // std_2.viewStudent();
+  ////////////////////////
+  Animal animal_1 = Animal();
+  animal_1.animalName = ("Prayut");
+  animal_1.animalType = ("Kuk");
+  animal_1.numberOfLeqs = (4);
+  var test_object = {animal_1.viewData(), animal_1.checkInsect()};
+  print(test_object);
+}
+
+class Animal {
+  String? animalName;
+  int numberOfLeqs = 0;
+  String? animalType;
+
+  viewData() => print("Animal name:${animalName} Type::${animalType}");
+
+  checkInsect() => (numberOfLeqs >= 8) ? print("แมง") : print("ไม่ใช่แมง");
 }
 
 class Student {
